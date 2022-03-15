fff6fa90f50f71a4e4043502d6f938b8b479aa70
AnjaBruls <anja@transceptor.technology>
2022-03-15 14:31:48 +0100

    Fix package loss

--- lib/check/checkPing.py
+++ lib/check/checkPing.py
@@ -26,7 +26,7 @@ class CheckPing(Base):
             min_time = itm.min_rtt * 1000  # float (s)
 
         return {
-            'droppedCount': itm.packet_loss,  # int
+            'droppedCount': itm.packet_sent - itm.packet_received,  # int
             'successCount': itm.packets_received,  # int
             'name': 'ping',  # unicode
             'maxTime': max_time,
