8d5ccee4e5ec4d8e5f9a95947b1d400c265a9aaf
Jeroen van der Heijden <jeroen@cesbit.com>
2022-04-06 09:58:12 +0200

    Upd version and requirements

--- lib/version.py
+++ lib/version.py
@@ -1,1 +1,4 @@
-__version__ = '2.0.0'
+# Version string. Examples:
+#  '2.0.0'
+#  '2.0.0-alpha0'
+__version__ = '2.0.1-alpha0'
--- pingprobe.py
+++ pingprobe.py
@@ -24,6 +24,8 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    AgentCoreClient.setup_logger(args.log_level, args.log_colorized)
+
     cl = AgentCoreClient(
         'pingProbe',
         __version__,
@@ -31,8 +33,6 @@ if __name__ == '__main__':
         None,
         '/data/config/pingprobe/pingProbe-config.json'
     )
-
-    cl.setup_logger(args.log_level, args.log_colorized)
 
     asyncio.get_event_loop().run_until_complete(
         cl.connect_loop()
